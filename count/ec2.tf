6d2ec25b6d9776bf548c44ddb973ca28ec670387
aruenjali <aruenjali@gmail.com>
2025-12-30 21:24:30 +0530

    terrafrom

--- count/ec2.tf
+++ count/ec2.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "terraform" {
-    count = 4
+    count = 4  ## no if instances
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_all.id]
